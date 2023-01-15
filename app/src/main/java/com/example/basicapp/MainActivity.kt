14afa27d4b016c6e4b910b76e1a5d230093e7722
Pankaj Mishra <pankaj_mishra@epam.com>
2023-01-15 23:24:24 +0530

    kotlin way added (fixed kotlin issue)

--- app/src/main/java/com/example/basicapp/MainActivity.kt
+++ app/src/main/java/com/example/basicapp/MainActivity.kt
@@ -8,6 +8,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
+// This line for all elements references
+import kotlinx.android.synthetic.main.activity_main.*
+
 // Starting/Entry path of application(MainActivity). (ex: index.js, app.js etc.)
 class MainActivity : AppCompatActivity() {
 // When app starts then "onCreate" function will trigger and execute all the methods inside "onCreate"
@@ -17,15 +20,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 //  find button using id and trigger onPress function
-    val displayText = findViewById(R.id.display) as TextView
-    val updateBtn = findViewById(R.id.update) as Button
-    val resetBtn = findViewById(R.id.reset) as Button
-
-    updateBtn.setOnClickListener{
-        displayText.text = "Good Bye Pankaj"
+//  (JAVA way)
+//    val displayText = findViewById(R.id.display) as TextView
+//    val updateBtn = findViewById(R.id.update) as Button
+//    val resetBtn = findViewById(R.id.reset) as Button
+//
+//    updateBtn.setOnClickListener{
+//        displayText.text = "Good Bye Pankaj"
+//    }
+//    resetBtn.setOnClickListener{
+//        displayText.text = "Welcome Back Pankaj"
+//    }
+//  (Kotlin way)
+    update.setOnClickListener{
+        show.text = "Good Bye Pankaj (PKM)"
     }
-    resetBtn.setOnClickListener{
-        displayText.text = "Welcome Back Pankaj"
+    reset.setOnClickListener{
+        show.text = "Welcome Back Pankaj (PKM)"
     }
     }
 }
